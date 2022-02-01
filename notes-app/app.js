e9aead92ca1a009d9ca0fa9e3b2a4b986634c918
Alex Sexwale <alexsexwale@gmail.com>
2022-02-01 02:39:48 +0200

    Core Modules

---
+++ notes-app/app.js
@@ -0,0 +1,12 @@
+// We are calling the required function to load the fs module (file system module)
+const fs = require('fs');
+// We are now able to use the fs function which allows us to create and write into a file
+
+// Create the notes.text file and write text in the file
+fs.writeFileSync('notes.txt', 'This file was created by Node.js');
+
+// This will overwrite the previous notes create previously above.
+fs.writeFileSync('notes.txt', 'Welcome to my first Node tutorial. ');
+
+// This will append (add on) the text in the notes.txt
+fs.appendFileSync('notes.txt', 'We are on our way to being a Node.js Developer');
